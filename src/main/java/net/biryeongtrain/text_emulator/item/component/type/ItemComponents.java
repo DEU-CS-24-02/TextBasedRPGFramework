0ac6cc0726be0a51410b35d48a443aa9af8eba82
20212936 <02kdh1109@naver.com>
2024-10-18 22:20:32 +0900

    Update src/main/java/net/biryeongtrain/text_emulator/item/component/type/ItemComponents.java
    Co-authored-by: biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>

--- src/main/java/net/biryeongtrain/text_emulator/item/component/type/ItemComponents.java
+++ src/main/java/net/biryeongtrain/text_emulator/item/component/type/ItemComponents.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.function.UnaryOperator;
 
 public class ItemComponents {
-    public enum Rarity{
+    public enum Rarity {
         COMMON,
         ADVANCED,
         RARE,
