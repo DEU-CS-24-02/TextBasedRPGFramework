9be75881040778ce7e65650b7bfe18d2e94854c1
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2024-11-20 21:58:59 +0900

    ADD : implement codec, lootTable class

--- src/main/java/net/biryeongtrain/text_emulator/entity/LootTableInstance.java
+++ src/main/java/net/biryeongtrain/text_emulator/entity/LootTableInstance.java
@@ -7,6 +7,7 @@ import net.biryeongtrain.text_emulator.item.ItemStack;
 import java.util.List;
 
 public record LootTableInstance(int weight, List<ItemStack> items) {
+    public static LootTableInstance EMPTY = new LootTableInstance(0, List.of(ItemStack.EMPTY));
     public static Codec<LootTableInstance> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.INT.fieldOf("weight").forGetter(LootTableInstance::weight),
             ItemStack.CODEC.listOf().fieldOf("items").forGetter(LootTableInstance::items)
