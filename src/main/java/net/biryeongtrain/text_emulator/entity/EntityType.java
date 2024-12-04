121f967e268d2a286f983a9c0150d8d370834e0e
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2024-12-04 23:46:51 +0900

    FIX : Make codec to initialize lazy

--- src/main/java/net/biryeongtrain/text_emulator/entity/EntityType.java
+++ src/main/java/net/biryeongtrain/text_emulator/entity/EntityType.java
@@ -6,19 +6,28 @@ import net.biryeongtrain.text_emulator.level.Scene;
 import net.biryeongtrain.text_emulator.registry.RegistryKey;
 import net.biryeongtrain.text_emulator.registry.RegistryKeys;
 
+import java.util.List;
+
+/**
+ * EntityType은 Entity 클래스를 생성하기 위한 정보를 가지고 있습니다.
+ * EntityType은 직접 생성되지 않으며, LoaderManager가 인식한 시나리오의 엔티티 정보 파일을 통해 등록됩니다.
+ * @see net.biryeongtrain.text_emulator.io.LoadManager
+ */
 public class EntityType {
-    public static Codec<EntityType> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+    public static Codec<EntityType> CODEC = Codec.lazyInitialized(() -> RecordCodecBuilder.create(instance -> instance.group(
             RegistryKey.createCodec(RegistryKeys.ENTITY_TYPE).fieldOf("key").forGetter(EntityType::getKey),
             Codec.FLOAT.fieldOf("defaultHealth").forGetter(EntityType::getDefaultHealth),
             Codec.FLOAT.fieldOf("defaultDamage").forGetter(EntityType::getDefaultDamage),
             Codec.FLOAT.fieldOf("defaultArmor").forGetter(EntityType::getDefaultArmor),
-            LootTableManager.CODEC.fieldOf("lootTableManager").forGetter(EntityType::getLootTableManager)
-    ).apply(instance, (EntityType::new)));
+            LootTableManager.CODEC.fieldOf("lootTableManager").forGetter(EntityType::getLootTableManager),
+            EntityTag.CODEC.listOf().fieldOf("tags").forGetter(EntityType::getTags)
+    ).apply(instance, EntityType::new)));
 
     private final RegistryKey<EntityType> key;
     private final float defaultHealth;
     private final float defaultDamage;
     private final float defaultArmor;
+    private final List<EntityTag> tags;
     private final LootTableManager lootTableManager;
 
     public LootTableManager getLootTableManager() {
@@ -37,15 +46,20 @@ public class EntityType {
         return defaultHealth;
     }
 
+    public List<EntityTag> getTags() {
+        return this.tags;
+    }
+
     public RegistryKey<EntityType> getKey() {
         return key;
     }
 
-    private EntityType(RegistryKey<EntityType> key, float defaultHealth, float defaultDamage, float defaultArmor, LootTableManager lootTableManager) {
+    private EntityType(RegistryKey<EntityType> key, float defaultHealth, float defaultDamage, float defaultArmor, LootTableManager lootTableManager, List<EntityTag> tags) {
         this.key = key;
         this.defaultHealth = defaultHealth;
         this.defaultDamage = defaultDamage;
         this.defaultArmor = defaultArmor;
+        this.tags = tags;
         this.lootTableManager = lootTableManager;
     }
 
