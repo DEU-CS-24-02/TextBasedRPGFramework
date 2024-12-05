c498a0b47df133d6236f7e0f64cf5be4f0e93e7d
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2024-12-05 22:57:22 +0900

    FIX : set some class public for testing.

--- src/test/java/IOTest.java
+++ src/test/java/IOTest.java
@@ -2,11 +2,15 @@ import com.google.gson.JsonElement;
 import com.mojang.serialization.JsonOps;
 import net.biryeongtrain.text_emulator.Main;
 import net.biryeongtrain.text_emulator.entity.EntityTag;
+import net.biryeongtrain.text_emulator.entity.EntityType;
+import net.biryeongtrain.text_emulator.entity.LootTableInstance;
+import net.biryeongtrain.text_emulator.entity.LootTableManager;
 import net.biryeongtrain.text_emulator.io.LoadManager;
 import net.biryeongtrain.text_emulator.io.storage.ScenarioPath;
 import net.biryeongtrain.text_emulator.item.Item;
 import net.biryeongtrain.text_emulator.item.ItemStack;
 import net.biryeongtrain.text_emulator.item.Items;
+import net.biryeongtrain.text_emulator.registry.Registries;
 import net.biryeongtrain.text_emulator.scenario.ScenarioMeta;
 import net.biryeongtrain.text_emulator.utils.Util;
 import net.biryeongtrain.text_emulator.utils.identifier.Identifier;
@@ -57,6 +61,7 @@ public class IOTest {
         Path path2 = Path.of("./scenarios/ccc/scenario.json");
         Path path3 = Path.of("./scenarios/aaa/scenario.json");
         Path entityTagPath = path.getParent().resolve("entity_tag/test_tag.json");
+        Path entityTypePath = path.getParent().resolve("entity/test_entity.json");
         ScenarioMeta meta = new ScenarioMeta("테스트 시나리오", "test_scenario", "1.0.0", "테스트 시나리오입니다.", List.of("biryeongtrain"), List.of("test_scenario2"));
         ScenarioMeta meta2 = new ScenarioMeta("테스트 시나리오2", "test_scenario2", "1.0.0", "테스트 시나리오입니다.", List.of("biryeongtrain"), List.of());
         ScenarioMeta meta3 = new ScenarioMeta(
@@ -95,14 +100,23 @@ public class IOTest {
                 Files.writeString(itemPath.resolve("test_item.json"), itemJson.toString(), StandardCharsets.UTF_8);
             }
 
+            EntityTag tag = new EntityTag("TestTag", Identifier.ofDefault("test_tag"), EntityTag.Type.ENTITY);
             if (!Files.exists(entityTagPath)) {
                 Files.createDirectories(entityTagPath.getParent());
-                EntityTag tag = new EntityTag("TestTag", Identifier.ofDefault("test_tag"), EntityTag.Type.ENTITY);
                 var tagJson = EntityTag.IO_CODEC.encodeStart(JsonOps.INSTANCE, tag).getOrThrow();
                 Files.writeString(entityTagPath, tagJson.toString());
             }
+
             CompletableFuture<Void> task = CompletableFuture.runAsync(manager::load, Util.IOExecutor);
             task.get();
+
+            // 해당 부분은 레지스트리에 파일이 등록되어야만 직렬화가 가능한 파일들입니다.
+            if (!Files.exists(entityTypePath)) {
+                Files.createDirectories(entityTypePath.getParent());
+                var entityType = new EntityType(Identifier.ofDefault("test_entity"), 20f, 20f, 20f, new LootTableManager(List.of(LootTableInstance.EMPTY)), List.of(Registries.ENTITY_TAG.get(tag.id)));
+                var entityTypeJson = EntityType.CODEC.encodeStart(JsonOps.INSTANCE, entityType).getOrThrow();
+                Files.writeString(entityTypePath, entityTypeJson.toString());
+            }
             Main.LOGGER.info("DONE!");
         } catch (IOException e) {
             Assertions.fail(e);
