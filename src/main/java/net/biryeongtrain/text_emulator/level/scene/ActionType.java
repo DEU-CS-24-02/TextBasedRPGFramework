342f4f6f3dc5ce8922aea255824100f8fc4f725a
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2024-12-05 23:02:08 +0900

    FIX : Enums Implements StringIdentifiable class for use Codec

--- src/main/java/net/biryeongtrain/text_emulator/level/scene/ActionType.java
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/ActionType.java
@@ -1,5 +1,13 @@
 package net.biryeongtrain.text_emulator.level.scene;
 
-public enum ActionType {
+import net.biryeongtrain.text_emulator.utils.collections.StringIdentifiable;
+
+public enum ActionType implements StringIdentifiable {
     GIVE, TAKE, PRINT, GOTO;
+
+    public static EnumCodec<ActionType> CODEC = StringIdentifiable.createCodec(ActionType::values);
+    @Override
+    public String asString() {
+        return this.name().toLowerCase();
+    }
 }
--- src/main/java/net/biryeongtrain/text_emulator/level/scene/Condition.java
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/Condition.java
@@ -1,5 +1,12 @@
 package net.biryeongtrain.text_emulator.level.scene;
 
-public enum Condition {
+import net.biryeongtrain.text_emulator.utils.collections.StringIdentifiable;
+
+public enum Condition implements StringIdentifiable {
     IF, NEVER, ALWAYS;
+
+    @Override
+    public String asString() {
+        return this.name().toLowerCase();
+    }
 }
