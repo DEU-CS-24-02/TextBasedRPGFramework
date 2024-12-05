3d475d01d2acd05795c658a00970585bede021b3
20232952 <1mybv@naver.com>
2024-12-05 22:23:31 +0900

    ADD : SceneAction class

---
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/SceneAction.java
@@ -0,0 +1,10 @@
+package net.biryeongtrain.text_emulator.level.scene;
+
+public class SceneAction {
+    ActionType type;
+    Unit unit;
+    String value;
+
+    public void execute() {}
+
+}
---
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/Unit.java
@@ -0,0 +1,5 @@
+package net.biryeongtrain.text_emulator.level.scene;
+
+public enum Unit {
+    KARMA, GOLD, HEALTH, ITEM;
+}
