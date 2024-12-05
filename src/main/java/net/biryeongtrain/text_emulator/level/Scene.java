35c290146fda93fc3e07d71cd7e7a64f60ab8ad6
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2024-12-05 23:54:43 +0900

    FIX : Scene Implementation

--- src/main/java/net/biryeongtrain/text_emulator/level/Scene.java
+++ src/main/java/net/biryeongtrain/text_emulator/level/Scene.java
@@ -1,7 +1,16 @@
 package net.biryeongtrain.text_emulator.level;
 
-import java.util.Random;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.biryeongtrain.text_emulator.level.scene.SceneDecision;
+import net.biryeongtrain.text_emulator.level.scene.SceneSelector;
 
-public interface Scene {
-    Random getRandom();
+import java.util.List;
+
+public record Scene(SceneSelector selector, List<String> conversations, SceneDecision decision) {
+    public static Codec<Scene> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            SceneSelector.CODEC.fieldOf("selector").forGetter(Scene::selector),
+            Codec.STRING.listOf().fieldOf("conversations").forGetter(Scene::conversations),
+            SceneDecision.CODEC.fieldOf("decision").forGetter(Scene::decision)
+    ).apply(instance, Scene::new));
 }
--- src/main/java/net/biryeongtrain/text_emulator/level/scene/SceneSelector.java
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/SceneSelector.java
@@ -8,14 +8,14 @@ public record SceneSelector(Condition condition,
                             Operator operator,
                             String value,
                             Unit unit,
-                            float weight) {
+                            int weight) {
 
     public static Codec<SceneSelector> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Condition.CODEC.fieldOf("condition").forGetter(SceneSelector::condition),
             Operator.CODEC.fieldOf("operator").forGetter(SceneSelector::operator),
             Codec.STRING.fieldOf("value").forGetter(SceneSelector::value),
             Unit.CODEC.fieldOf("unit").forGetter(SceneSelector::unit),
-            Codec.FLOAT.fieldOf("weight").forGetter(SceneSelector::weight)
+            Codec.INT.fieldOf("weight").forGetter(SceneSelector::weight)
     ).apply(instance, SceneSelector::new));
 
 
