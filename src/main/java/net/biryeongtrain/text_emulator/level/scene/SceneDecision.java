abb7dce6dee35fadbdda4e0f93e0465dee32cb97
20232952 <1mybv@naver.com>
2024-12-05 22:25:04 +0900

    ADD : SceneDecision class

---
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/SceneDecision.java
@@ -0,0 +1,13 @@
+package net.biryeongtrain.text_emulator.level.scene;
+
+import net.biryeongtrain.text_emulator.utils.identifier.Identifier;
+
+import java.util.List;
+
+public class SceneDecision {
+    String text;
+    String description;
+    List<SceneAction> actions;
+
+    public void  createButton() {}
+}
