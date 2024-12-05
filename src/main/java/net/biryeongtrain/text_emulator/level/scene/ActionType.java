ed6779e3c96bf0b7ce923c5d59dd6216622c8281
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2024-12-05 23:13:16 +0900

    ADD : ActionType 에 추상 메소드 execute 추가

--- src/main/java/net/biryeongtrain/text_emulator/level/scene/ActionType.java
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/ActionType.java
@@ -3,9 +3,32 @@ package net.biryeongtrain.text_emulator.level.scene;
 import net.biryeongtrain.text_emulator.utils.collections.StringIdentifiable;
 
 public enum ActionType implements StringIdentifiable {
-    GIVE, TAKE, PRINT, GOTO;
+    GIVE {
+        @Override
+        public void execute(Unit unit, String value) {
+
+        }
+    }, TAKE {
+        @Override
+        public void execute(Unit unit, String value) {
+
+        }
+    }, PRINT {
+        @Override
+        public void execute(Unit unit, String value) {
+
+        }
+    }, GOTO {
+        @Override
+        public void execute(Unit unit, String value) {
+
+        }
+    };
+
+    public abstract void execute(Unit unit, String value);
 
     public static EnumCodec<ActionType> CODEC = StringIdentifiable.createCodec(ActionType::values);
+
     @Override
     public String asString() {
         return this.name().toLowerCase();
