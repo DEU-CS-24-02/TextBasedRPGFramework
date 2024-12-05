36a94e8990d8d1c9bf3bc3e424d94614f64c1d0b
biryeongtrain <83863154+biryeongtrain@users.noreply.github.com>
2024-12-05 23:12:40 +0900

    FIX : 클래스들 모두 레코드 타입으로 변경(불변 객체임.)

--- src/main/java/net/biryeongtrain/text_emulator/level/scene/Condition.java
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/Condition.java
@@ -5,6 +5,7 @@ import net.biryeongtrain.text_emulator.utils.collections.StringIdentifiable;
 public enum Condition implements StringIdentifiable {
     IF, NEVER, ALWAYS;
 
+    public static EnumCodec<Condition> CODEC = StringIdentifiable.createCodec(Condition::values);
     @Override
     public String asString() {
         return this.name().toLowerCase();
--- src/main/java/net/biryeongtrain/text_emulator/level/scene/SceneAction.java
+++ src/main/java/net/biryeongtrain/text_emulator/level/scene/SceneAction.java
@@ -1,10 +1,21 @@
 package net.biryeongtrain.text_emulator.level.scene;
 
-public class SceneAction {
-    ActionType type;
-    Unit unit;
-    String value;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import org.checkerframework.checker.units.qual.C;
 
-    public void execute() {}
+public record SceneAction(ActionType type,
+                          Unit unit,
+                          String value) {
+
+    public static Codec<SceneAction> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            ActionType.CODEC.fieldOf("type").forGetter(SceneAction::type),
+            Unit.CODEC.fieldOf("unit").forGetter(SceneAction::unit),
+            Codec.STRING.fieldOf("value").forGetter(SceneAction::value)
+    ).apply(instance, SceneAction::new));
+
+    public void execute() {
+        this.type.execute(this.unit, this.value);
+    }
 
 }
