ca27e39115d8c9399a7d3f08cb17757af9b33213
20232952 <1mybv@naver.com>
2024-12-11 07:34:45 +0900

    ADD: Panels in SimpleTextRPG class
    플레이어 정보, 인벤토리 와 씬 정보, 엔티티 정보를 담을 패널들을 만들었다

---
+++ src/main/java/net/biryeongtrain/text_emulator/swing/PlayerInventoryPanel.java
@@ -0,0 +1,11 @@
+package net.biryeongtrain.text_emulator.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerInventoryPanel extends JPanel {
+    public PlayerInventoryPanel() {
+        setBackground(Color.BLACK);
+
+    }
+}
--- src/main/java/net/biryeongtrain/text_emulator/swing/SimpleTextRPG.java
+++ src/main/java/net/biryeongtrain/text_emulator/swing/SimpleTextRPG.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class SimpleTextRPG extends JFrame {
-    Dimension centerSize = new Dimension(640, 680);
+    Dimension centerSize = new Dimension(640, 685);
 
     public SimpleTextRPG() {
         // 창 설정
@@ -14,11 +14,27 @@ public class SimpleTextRPG extends JFrame {
 
         Container c = getContentPane();
 
-        //텍스트 패널 위치 및 크기 설정
+        // 텍스트 패널 위치 및 크기 설정
         TextAreaPanel textAreaPanel = new TextAreaPanel();
         textAreaPanel.setSize(centerSize);
         textAreaPanel.setLocation((1280 - centerSize.width) / 2,0);
         c.add(textAreaPanel);
+
+        // 플레이어 정보 패널 크기 설정
+        JPanel PlayerPanel = new JPanel(new GridLayout(2,1,1,1));
+        PlayerPanel.setSize(centerSize.width/2 - 1, centerSize.height);
+        add(PlayerPanel);
+        PlayerPanel.add(new PlayerInfoPanel());
+        PlayerPanel.add(new PlayerInventoryPanel());
+
+        // 씬 및 엔티티 정보 패널 위치 및 크기 설정
+        JPanel ScenePanel = new JPanel(new GridLayout(2,1,1,1));
+        ScenePanel.setSize(centerSize.width/2 - 1, centerSize.height);
+        ScenePanel.setLocation(1280 - centerSize.width / 2 +1,0);
+        add(ScenePanel);
+        ScenePanel.add(new SceneInfoPanel());
+        ScenePanel.add(new EntityInfoPanel());
+
 
         // 창 크기 설정 및 표시
         setSize(1280, 720);
